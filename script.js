83dbc572685e4f60cadac7f03dc1a8fee65bc7d2
nimashakonara <119902763+nimashakonara@users.noreply.github.com>
2025-06-30 14:38:54 +0900

    Add files via upload
    Adding index.js style.css & script.js, Adding Images

---
+++ script.js
@@ -0,0 +1,26 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+    showPopup(true); //Show the popup
+    
+    var element = document.createElement("div");
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
+    
+    document.getElementById("all_recommendations").appendChild(element); 
+    recommendation.value = ""; //Reset the textarea
+
+}
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
